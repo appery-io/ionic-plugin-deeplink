237d057918ace0c19b740b25e6eebebcd99ec69c
Oleg Danchenkov <odanchenkov@exadel.com>
2026-09-10 11:59:24 +0300

    fix(ios): deliver Universal Links on Cordova iOS 8 SceneDelegate  - Listen for CDVPluginContinueUserActivityNotification and forward cold-start userActivities from CDVSceneDelegate, which AppDelegate hooks no longer receive.

--- src/ios/IonicDeeplinkPlugin.m
+++ src/ios/IonicDeeplinkPlugin.m
@@ -1,11 +1,76 @@
 #import "IonicDeeplinkPlugin.h"
 
+#import <objc/runtime.h>
 #import <Cordova/CDVAvailability.h>
+#import <Cordova/CDVSceneDelegate.h>
+
+static NSString *const kCDVContinueUserActivityNotification = @"CDVPluginContinueUserActivityNotification";
+static NSURL *IonicDeeplinkPendingURL = nil;
+
+@interface CDVSceneDelegate (IonicDeeplink)
+- (void)ionicdeeplink_scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions;
+- (void)scene:(UIScene *)scene continueUserActivity:(NSUserActivity *)userActivity;
+@end
+
+@implementation CDVSceneDelegate (IonicDeeplink)
+
++ (void)load {
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    Method original = class_getInstanceMethod(self, @selector(scene:willConnectToSession:options:));
+    Method swizzled = class_getInstanceMethod(self, @selector(ionicdeeplink_scene:willConnectToSession:options:));
+    if (original && swizzled) {
+      method_exchangeImplementations(original, swizzled);
+    }
+  });
+}
+
+- (void)ionicdeeplink_scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
+  [self ionicdeeplink_scene:scene willConnectToSession:session options:connectionOptions];
+  for (NSUserActivity *activity in connectionOptions.userActivities) {
+    [self scene:scene continueUserActivity:activity];
+  }
+}
+
+@end
 
 @implementation IonicDeeplinkPlugin
 
++ (void)load {
+  // Capture Universal Links even if the plugin instance is not created yet
+  // (cold start on Cordova iOS 8+ SceneDelegate).
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(captureContinueUserActivity:)
+                                               name:kCDVContinueUserActivityNotification
+                                             object:nil];
+}
+
++ (void)captureContinueUserActivity:(NSNotification *)notification {
+  NSUserActivity *userActivity = [notification object];
+  if (![userActivity isKindOfClass:[NSUserActivity class]]) {
+    return;
+  }
+  if (![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
+    return;
+  }
+  IonicDeeplinkPendingURL = userActivity.webpageURL;
+  NSLog(@"IonicDeepLinkPlugin: captured pending Universal Link %@", IonicDeeplinkPendingURL);
+}
+
 - (void)pluginInitialize {
   _handlers = [[NSMutableArray alloc] init];
+
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(handleContinueUserActivityNotification:)
+                                               name:kCDVContinueUserActivityNotification
+                                             object:nil];
+
+  if (IonicDeeplinkPendingURL) {
+    NSLog(@"IonicDeepLinkPlugin: consuming pending Universal Link %@", IonicDeeplinkPendingURL);
+    _lastEvent = [self createResult:IonicDeeplinkPendingURL];
+    [self sendToJs];
+    IonicDeeplinkPendingURL = nil;
+  }
 }
 
 /* ------------------------------------------------------------- */
@@ -33,6 +98,21 @@
   [_handlers addObject:command.callbackId];
   // Try to consume any events we got before we were listening
   [self sendToJs];
+}
+
+- (void)handleOpenURL:(NSNotification *)notification {
+  NSURL *url = [notification object];
+  if ([url isKindOfClass:[NSURL class]]) {
+    NSLog(@"IonicDeepLinkPlugin: handleOpenURL %@", url);
+    [self handleLink:url];
+  }
+}
+
+- (void)handleContinueUserActivityNotification:(NSNotification *)notification {
+  NSUserActivity *userActivity = [notification object];
+  if ([userActivity isKindOfClass:[NSUserActivity class]]) {
+    [self handleContinueUserActivity:userActivity];
+  }
 }
 
 - (BOOL)handleLink:(NSURL *)url {
@@ -87,7 +167,7 @@
 
   [self sendToJs];
 
-  return NO;
+  return YES;
 }
 
 - (void) sendToJs {
